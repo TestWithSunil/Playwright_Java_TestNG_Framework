cc829a901cea7c3b07f3fafe7190eab4588bcb17
sunilkva <sunilkva@CSGVTVLT207.creative.local>
2025-03-21 10:08:11 +0530

    change in base class

--- src/test/java/com/qa/demowebshop/base/BaseTest.java
+++ src/test/java/com/qa/demowebshop/base/BaseTest.java
@@ -1,53 +1,3 @@
-//package com.qa.demowebshop.base;
-//
-//import java.util.Properties;
-//
-//import org.testng.Assert;
-//import org.testng.annotations.AfterTest;
-//import org.testng.annotations.BeforeTest;
-//import org.testng.annotations.Parameters;
-//
-//import com.microsoft.playwright.Page;
-//import com.qa.demowebshop.factory.PlaywrightFactory;
-//import com.qa.demowebshop.pages.HomePage;
-//import com.qa.demowebshop.pages.LoginPage;
-//
-//public class BaseTest {
-//	PlaywrightFactory pf;
-//	Page page;
-//	protected Properties prop;
-//	protected HomePage homepage;
-//	protected LoginPage loginpage;
-//	
-//	@Parameters({"browser"})
-//	@BeforeTest
-//	public void setup(String browserName) {
-//		pf = new PlaywrightFactory();
-//		prop = pf.init_prop();
-//
-//		
-//	    // If browserName is not provided from XML, use the one from config.properties
-//	    if (browserName != null && !browserName.isEmpty()) {
-//	        prop.setProperty("browser", browserName);
-//	        System.out.println("Browser name from XML file: " + browserName);
-//	    } else {
-//	        browserName = prop.getProperty("browser"); // Default from config.properties
-//	        System.out.println("No browser name provided from XML. Using default from config.properties: " + browserName);
-//	    }
-//	    
-//		page = pf.initBrowser(prop);
-//		homepage = new HomePage(page);
-//	}
-//	
-//	@AfterTest
-//	public void teardown() {
-//		
-//		if (page != null) {
-//			page.context().browser().close();
-//        }
-//	}
-//}
-
 package com.qa.demowebshop.base;
 
 import java.util.Properties;
@@ -71,7 +21,8 @@ public class BaseTest {
 
     @Parameters({"browser"})
     @BeforeTest
-    public void setup(@Optional("") String browserName) {  // Allows TestNG to handle missing parameter
+    public void setup(@Optional("") String browserName) {  
+        System.out.println("Initializing Playwright Factory...");
         pf = new PlaywrightFactory();
         prop = pf.init_prop();
 
@@ -85,14 +36,21 @@ public class BaseTest {
 
         prop.setProperty("browser", browserName); // Set the browser value in properties
         page = pf.initBrowser(prop);
+
+        // Null Check to prevent NPE when creating HomePage
+        if (page == null) {
+            throw new IllegalStateException("Browser page failed to initialize. Check Playwright setup.");
+        }
+
+        System.out.println("Browser initialized successfully. Creating HomePage instance...");
         homepage = new HomePage(page);
     }
 
     @AfterTest
     public void teardown() {
-        if (page != null) {
-            page.context().browser().close();
+        System.out.println("Closing the browser...");
+        if (pf != null) {
+            pf.cleanup(); // Ensures Playwright and BrowserContext are properly closed
         }
     }
 }
-
